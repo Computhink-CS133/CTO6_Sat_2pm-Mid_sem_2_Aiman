8dc7b087e093776a42533fb92161e33ad1687b64
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-30 06:37:19 +0000

    Aug 30, 2025, 6:37 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,3 +2,11 @@ function setup(){
     createCanvas(600,600);
     background(100);
 }
+function draw(){
+    fill(0,0,0);
+    stroke(255,0,0);
+    strokeWeight(10);
+    circle(150,200,100);
+    triangle(300,150,250,250,350,250);
+    rect(400,150,100,100); 
+}
