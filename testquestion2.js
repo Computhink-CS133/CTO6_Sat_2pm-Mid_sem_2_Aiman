2b0a2d10fd6b30e64662d48ba4baf7de510f9b50
Computhink-CS133 <cs133@computhink.com.sg>
2025-08-30 06:37:51 +0000

    Aug 30, 2025, 6:37 AM

--- testquestion2.js
+++ testquestion2.js
@@ -5,9 +5,6 @@ function setup(){
 var colour=0
 function draw(){
     fill(colour);
-    stroke(255,0,0);
-    strokeWeight(10);
     circle(150,200,100);
-    triangle(300,150,250,250,350,250);
-    rect(400,150,100,100); 
+
 }
